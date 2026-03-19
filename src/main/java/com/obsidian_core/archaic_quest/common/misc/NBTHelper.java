ec1da3b1b3b8e9a93600fc6e26aab6e7d35cb562
Sarinsa <mikkemusbruker@gmail.com>
2026-03-19 20:14:48 +0100

    Some NBT convenience stuff

--- src/main/java/com/obsidian_core/archaic_quest/common/misc/NBTHelper.java
+++ src/main/java/com/obsidian_core/archaic_quest/common/misc/NBTHelper.java
@@ -2,6 +2,7 @@ package com.obsidian_core.archaic_quest.common.misc;
 
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -32,8 +33,25 @@ public class NBTHelper {
         return blockTag;
     }
     
+    /** Helper method for getting around some annoying type parameter shenanigans. */
     @SuppressWarnings( "unchecked" )
     private static <T extends Comparable<T>> String getName( Property<T> property, Comparable<?> comparable ) {
         return property.getName( (T) comparable );
+    }
+    
+    /**
+     * Fetches and returns the compound tag in the parent tag with the
+     * specified name, if it exists. Otherwise, a new compound tag is created,
+     * put in the parent tag and returned.
+     */
+    public static CompoundTag getOrCreate( CompoundTag parentTag, String tagName ) {
+        if( !parentTag.contains( tagName, Tag.TAG_COMPOUND ) ) {
+            CompoundTag tag = new CompoundTag();
+            parentTag.put( tagName, tag );
+            return tag;
+        }
+        else {
+            return parentTag.getCompound( tagName );
+        }
     }
 }
