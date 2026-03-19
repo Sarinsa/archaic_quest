a9b155418bca733b9d3c1ac9bca2b0e3bc78f405
Sarinsa <mikkemusbruker@gmail.com>
2026-03-19 20:14:56 +0100

    whoops

--- src/main/java/com/obsidian_core/archaic_quest/client/screen/KnappingTableScreen.java
+++ src/main/java/com/obsidian_core/archaic_quest/client/screen/KnappingTableScreen.java
@@ -1,18 +1,18 @@
 package com.obsidian_core.archaic_quest.client.screen;
 
 import com.obsidian_core.archaic_quest.common.core.ArchaicQuest;
-import com.obsidian_core.archaic_quest.common.inventory.container.KnappingTableContainer;
+import com.obsidian_core.archaic_quest.common.inventory.menu.KnappingTableMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
-public class KnappingTableScreen extends AbstractContainerScreen<KnappingTableContainer> {
+public class KnappingTableScreen extends AbstractContainerScreen<KnappingTableMenu> {
     
     private static final ResourceLocation texture = ArchaicQuest.rl( "textures/gui/knapping_table.png" );
     
-    public KnappingTableScreen( KnappingTableContainer container, Inventory inventory, Component title ) {
+    public KnappingTableScreen( KnappingTableMenu container, Inventory inventory, Component title ) {
         super( container, inventory, title );
     }
     
