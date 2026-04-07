f9e490f10bfff2473969e85f07a48204dd95cc69
Sarinsa <mikkemusbruker@gmail.com>
2026-04-08 00:29:39 +0200

    Removed debug thingy

--- src/main/java/com/obsidian_core/archaic_quest/common/item/blockitem/AztecThroneBlockItem.java
+++ src/main/java/com/obsidian_core/archaic_quest/common/item/blockitem/AztecThroneBlockItem.java
@@ -9,7 +9,6 @@ import net.minecraft.core.Direction;
 import net.minecraft.core.Vec3i;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 
 import java.util.function.Consumer;
@@ -23,7 +22,6 @@ public class AztecThroneBlockItem extends DirectionalMultiBlockItem {
                 PlaceableConstruct.builder()
                         .add( block.defaultBlockState().setValue( AztecThroneBlock.MASTER, true ), Vec3i.ZERO )
                         .add( block.defaultBlockState().setValue( AztecThroneBlock.PART, AztecThroneBlock.Part.BACK ), 0, 0, -1 )
-                        .add( Blocks.DIRT, 0, 0, -1 )
                         .add( block.defaultBlockState().setValue( AztecThroneBlock.PART, AztecThroneBlock.Part.BACK ), 0, 1, -1 )
                         .add( block.defaultBlockState().setValue( AztecThroneBlock.PART, AztecThroneBlock.Part.UPPER_BACK ), 0, 2, -1 )
                         .add( block.defaultBlockState().setValue( AztecThroneBlock.PART, AztecThroneBlock.Part.ARM ), -1, 0, 0 )
